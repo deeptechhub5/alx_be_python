79a32d23f99546936a4a5d59f7a4ba50dd69a29f
Emmanuel Fiifi Bentsi Essuman <emmanuelfiifibentsiessuman@gmail.com>
2025-09-11 02:12:50 +0000

    feat: Add simple_interst.py script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# Define the variables
+principal = 1000
+rate = 0.05
+time = 3
+# Calculate simple interest
+interest = principal * rate * time
+# Print the result in the specified format
+print(f"The simple interst is: {interest}")
