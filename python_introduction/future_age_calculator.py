55c3d98eab5366d5abad5befd9639f799288bd60
Emmanuel Fiifi Bentsi Essuman <emmanuelfiifibentsiessuman@gmail.com>
2025-09-11 02:41:54 +0000

    feat: Add future_age_calculator.py script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# Get user input for current age
+current_age = int(input("How old are you?"))
+# Calculate age in 2050 (assuming current year is 2023)
+future_age = current_age + 27
+# Print the result
+print(f"In 2050, you will be {future_age} years old.")
+
