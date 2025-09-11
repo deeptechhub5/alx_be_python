343fffb4b72653ed0c5571bc6e7195d892bf88f9
Emmanuel Fiifi Bentsi Essuman <emmanuelfiifibentsiessuman@gmail.com>
2025-09-11 02:20:14 +0000

    feat: Add rectangle_area.py script

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+# Define the variables
+length = 10
+width = 5
+# calculate the area 
+area = length * width
+# Print the result in the specified format
+print(f"The area of the rectangle is: {area}")
