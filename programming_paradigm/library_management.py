30210d6e4418413a36218c7a77e0574a9fa89789
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-10-04 05:01:22 +0000

    feat: added library_management.py script

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,58 @@
+class Book:
+    """Represents a single book with title, author, and checkout status."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out if available."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False  # already checked out
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False  # was not checked out
+
+    def is_available(self):
+        """Return True if the book is available, False otherwise."""
+        return not self._is_checked_out
+
+
+class Library:
+    """Represents a library that manages a collection of books."""
+
+    def __init__(self):
+        self._books = []  # private list of Book instances
+
+    def add_book(self, book):
+        """Add a Book instance to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False  # not found or unavailable
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False  # not found or not checked out
+
+    def list_available_books(self):
+        """Print all available books in the library."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
